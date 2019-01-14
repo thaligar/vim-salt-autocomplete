946330d327a814691ee20391b2ecd38080dfcfee
Sebastian Schmittner <stp.schmittner@gmail.com>
2019-01-15 00:04:59 +0100

    Awesome partial implementation

--- salt-autocomplete.py
+++ salt-autocomplete.py
@@ -3,6 +3,7 @@
 import os
 import ast
 import astunparse
+import yaml
 
 path="/home/sebastian/source/salt-stack/salt/states/"
 extension=".py"
@@ -61,44 +62,69 @@ for filename in [f for f in os.listdir(path) if f.endswith(extension)]:
     with open(path + filename, 'r') as item:
         source = item.read()
     print(filename)
-    states['name'] = filename[:-3]
-    states['methods'] = []
+    current = {}
+    current['name'] = filename[:-3]
+    current['methods'] = []
     tree = ast.parse(source)
     for stmt in ast.walk(tree):
         if isinstance(stmt, ast.FunctionDef):
             if not stmt.name.startswith("_"):
                 fn = {}
-                print('  ' + stmt.name)
+#                print('  ' + stmt.name)
                 fn['name'] = stmt.name
                 fn['args'] = []
                 fn['docs'] = get_description(stmt)
-                print('  * ' + fn['docs'])
+                #print('  * ' + fn['docs'])
 
                 args = stmt.args.args
                 defs = stmt.args.defaults
-            
-                print(len(args))
-                print(len(defs))
-                print(len(args)-len(defs))
+
+#                print(len(args))
+#                print(len(defs))
+#                print(len(args)-len(defs))
                 difference = len(args) - len(defs)
 
                 for i, a in enumerate(stmt.args.args):
-                    
+
                     defix = i - difference
 
                     if defix >= 0:
                         node = defs[defix]
                         translator = dispatcher[type(node)]
                         value_as_string = str(translator(node))
-                        fn['args'].append(a.arg + ': ' + value_as_string)
-                        print('    ' + a.arg + ': ' + value_as_string)
+                        fn['args'].append((a.arg,value_as_string))
+                        #print('    ' + a.arg + ': ' + value_as_string)
                     else:
-                        fn['args'].append(a.arg)
-                        print('    ' + a.arg)
+                        fn['args'].append((a.arg, None))
+                        #print('    ' + a.arg)
 
-                states['methods'].append(fn)
+                current['methods'].append(fn)
+    states[filename] = current
 
+def arg_line(arg, n):
+    if arg[1] is not None:
+        return '  - {}: ${{{}:{}}}\n'.format(arg[0], n, arg[1])
+    else:
+        return '  - {}: ${{{}}}\n'.format(arg[0], n)
 
+def generate_argument_list(args):
+    arg_list = ''
+    for index, arg in enumerate(args, 1):
+        arg_list = arg_list + arg_line(arg, index)
+    return arg_list
+
+state_keys = states.keys()
+
+for state_key in state_keys:
+    state = states[state_key]
+    print(yaml.safe_dump(state))
+    for method in state['methods']:
+        head = 'snippet {}.{} "{}"\n'.format(state['name'], method['name'], method['docs'])
+        first_line = '{}.{}:\n'.format(state['name'], method['name'])
+        arguments = generate_argument_list(method['args'])
+
+        entry = head + first_line + arguments + 'endsnippet\n'
+        print(entry)
 
 
 
